45b7a7b39a48bf2b088691ebc9d3fbd2a4d4ddcc
Chris Lane <chris@chris-allen-lane.com>
2018-12-19 14:16:45 -0500

    Updated to `wit-cms` v5.0.0

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Wit     = require('wit-cms');
 const express = require('express');
 const favicon = require('serve-favicon');
-const hljs    = require('highlight.js');
 const lodash  = require('lodash');
 const logger  = require('morgan');
 const path    = require('path');
@@ -12,14 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
-
-// only use the static-file middleware on the dev environment
-if ('development' == app.get('env')) {
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use('/media', express.static('media'));
-}
 
 // wit configs
 const config = {
@@ -33,51 +26,34 @@ const config = {
     appStart : Date.now(), // for stylesheet cache-busting
   },
 
-  // syntax highlighting configuration
-  remarkable: {
-    highlight: function (str, lang) {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(lang, str).value;
-        } catch (err) {}
-
-      }
-      try {
-        return hljs.highlightAuto(str).value;
-      } catch (err) {}
-      return '';
-    }
+  // specify the home page
+  pages: {
+    home: '/about',
   },
+
 };
 
-// init the app
-Wit(app, config, function(err, wit) {
+// initialize the app
+const wit = Wit(app, config);
 
-  // extract years for blog archives
-  wit.params.years = lodash(wit.posts)
-    .map('date.year')
-    .uniq()
-    .sort()
-    .reverse()
-    .value();
-  
-  // redirect the home page to the blog
-  app.get('/', function(req, res) {
-    res.redirect(301, '/blog');
+// extract years for blog archives
+wit.params.years = lodash(wit.posts)
+  .map('date.year')
+  .uniq()
+  .sort()
+  .reverse()
+  .value();
+
+// implement a 404 page
+app.use(function(req, res, next) {
+  res.status(404).render('404', {
+    page : {
+      title     : 'Not Found',
+      name      : 'Not Found',
+      content   : '<p>Not Found</p>',
+    },
+    wit: wit,
   });
-
-  // implement a 404 page
-  app.use(function(req, res, next) {
-    res.status(404).render('404', {
-      page : {
-        title     : 'Not Found',
-        name      : 'Not Found',
-        content   : '<p>Not Found</p>',
-      },
-      wit: wit,
-    });
-  });
-
 });
 
 module.exports = app;
